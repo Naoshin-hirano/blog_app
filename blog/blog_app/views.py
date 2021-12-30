ebb67d4a5f8ea9184600ada545632700fbe644d6
Hirano <hnaoya1120@gmail.com>
2021-12-30 17:31:14 +0900

    mediaフォルダでimg管理対応

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
+from .models import Post, Tag
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog_app/index.html')
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog_app/index.html', {'posts': posts})
