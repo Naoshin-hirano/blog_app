54a856545535fb9cb262e97db8312371c2626a39
Hirano <hnaoya1120@gmail.com>
2021-12-30 17:48:45 +0900

    Detail機能実装

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,7 +1,8 @@
-from .views import index
+from .views import index, detail
 from django.urls import path, include
 
 app_name = 'blog_app'
 urlpatterns = [
     path('', index, name='index'),
+    path('detail/<int:post_id>/', detail, name='detail'),
 ]
