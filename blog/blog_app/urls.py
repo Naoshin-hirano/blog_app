830296d32ea684d1f32b70eaf8d0aa8f103fee55
Hirano <hnaoya1120@gmail.com>
2021-12-31 18:02:01 +0900

    bootstrap導入してヘッダー追加

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,4 +1,4 @@
-from .views import index, detail, add
+from .views import index, detail, add, edit, delete
 from django.urls import path, include
 
 app_name = 'blog_app'
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('detail/<int:post_id>/', detail, name='detail'),
     path('add/', add, name='add'),
+    path('edit/<int:post_id>/', edit, name='edit'),
+    path('delete/<int:post_id>/', delete, name='delete'),
 ]
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -4,7 +4,26 @@ from .models import Post, Tag
 from .forms import PostAddForm
 
 # Create your views here.
+def delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('blog_app:index')
+
+def edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        #instance:オブジェクトとほぼ同義でクラスを使用したデータの実体
+        form = PostAddForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_app:detail', post_id=post.id)
+    else:
+        form = PostAddForm(instance=post)
+    #formにpostのデータを入れた状態のデータをadd.htmlに入れる
+    return render(request, 'blog_app/edit.html', {'form': form, 'post': post})
+
 def add(request):
+    #url/へ遷移してからPOSTでリクエスト出したとき
     if request.method == "POST":
         #リクエストがPOSTなら、formの中身とユーザーを更新してレスポンス
         form = PostAddForm(request.POST, request.FILES)#FILESは画像などのファイルデータ
@@ -15,9 +34,12 @@ def add(request):
             #記事の内容とuserの判別に成功したら保存
             post.save()
             return redirect('blog_app:index')
+    #url/へ遷移してからPOST以外のリクエスト出したとき
     else:
         #リクエストがPOST以外なら,特になにも更新せずにレスポンスに返す
         form = PostAddForm()
+    #url/へ遷移してなにもしてない状態
+    #formにpostを入れていない空の状態のデータをadd.htmlに入れる
     return render(request, 'blog_app/add.html', {'form': form})
 
 def detail(request, post_id):
