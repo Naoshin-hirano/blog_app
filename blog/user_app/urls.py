6ca1f10b294f645239bcbd944f06205fea68b77b
Hirano <hnaoya1120@gmail.com>
2022-01-02 14:48:20 +0900

    git commit -m signup機能実装

--- blog/user_app/urls.py
+++ blog/user_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import signup
 from django.contrib.auth import views as auth_views
 
 app_name = 'user_app'
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='user_app/login.html'), name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='user_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', signup, name='signup'),
 ]
